69293edcdcabf45fb90b51d4d6f71696d541b16e
Andrew Maytin <akm111@case.edu>
2024-08-02 15:08:14 -0500

    edit launch scripts

--- DNA/files/prelaunch_btree_chromo.sh
+++ DNA/files/prelaunch_btree_chromo.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # set the location of the btree_chromo files
-btree_chromo_files='/projects/bbsv/brg4/btree_chromo_files'
+btree_chromo_files='/projects/bddt/DNA/files'
 
 # set the main directory for the project/workshop and the user's subdirectory
-project_dir='/projects/bbsv'
+project_dir='/projects/bddt'
 user_subdir=${project_dir}/${USER}
 
 # create a workspace for btree_chromo
