62825ba917ce295cf22c7af698bb9b780de4837f
Tianyu Wu <41635675+forxhunter@users.noreply.github.com>
2025-07-24 09:09:37 -0500

    Update launch_vmd.sh

--- RDME/launch_vmd.sh
+++ RDME/launch_vmd.sh
@@ -3,7 +3,7 @@
 # This script is used to launch VMD
 
 cd /projects/beyi/
-module load 
+module load anaconda3_cpu
 source activate /projects/beyi/sw/conda/envs/vmdplugin
 module load vmd/1.9.4lm
 export LD_LIBRARY_PATH="$CONDA_PREFIX/lib:$LD_LIBRARY_PATH"
